ed7c761b489ed9b594a3b74c1126a09b24e2cbb4
gioiascari <gioia.ascari96@gmail.com>
2022-05-20 16:40:20 +0200

    es completato

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -5,14 +5,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Model Controller</title>
 </head>
 <body>
 
     <div class="container">
         <div class="card">
-            @foreach ( $movie as $prova )
-                <p>{{$prova['title']}}</p>
+            @foreach ( $movie as $data )
+                <h3>{{$data['title']}}</h3>
+                <h1>{{$data['original_title']}}</h1>
+                <p>{{$data['nationality']}}</p>
+                <p>{{$data['date']}}</p>
+                <p>{{$data['vote']}}</p>
+
+
 
 
             @endforeach
