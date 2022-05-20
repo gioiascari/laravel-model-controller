2860414c1e991ac830a4b1928a1246f8f7ba6d77
gioiascari <gioia.ascari96@gmail.com>
2022-05-20 16:21:19 +0200

    collegato app\movie al controller ed inserito i dati

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class PageController extends Controller
 {
-    //
+    public function index(){
+        $movie = Movie::all();
+        return view ('movie', compact('movie'));
+    }
 }
