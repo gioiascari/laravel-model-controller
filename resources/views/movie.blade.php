2b0f6c8d8ae46e719f9de9ba7697a72fbb442c5d
gioiascari <gioia.ascari96@gmail.com>
2022-05-20 18:13:31 +0200

    add some style

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -5,24 +5,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/app.css">
     <title>Model Controller</title>
 </head>
 <body>
 
+
+
+
+
     <div class="container">
-        <div class="card">
-            @foreach ( $movie as $data )
-                <h3>{{$data['title']}}</h3>
-                <h1>{{$data['original_title']}}</h1>
-                <p>{{$data['nationality']}}</p>
-                <p>{{$data['date']}}</p>
-                <p>{{$data['vote']}}</p>
+                <div class="title">
+                    <h1>Laravel Model Controller</h1>
+                </div>
+                <div class="card-container">
+                    @foreach ( $movie as $data )
+                    <div class="card">
+                        <div class="card-title">
+                            <h3>{{$data['title']}}</h3>
+                            <h1>{{$data['original_title']}}</h1>
+
+                        </div>
+                        <div class="card-detail">
+                            <p>{{$data['nationality']}}</p>
+                            <p>{{$data['date']}}</p>
+                            <p>{{$data['vote']}}</p>
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
 
 
 
-
-            @endforeach
-        </div>
     </div>
 </body>
 </html>
